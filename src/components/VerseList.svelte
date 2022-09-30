a1b2ba8b4b37838ec05df56e9c046c8d8feb6567
Mirko Hecky <m.hecky@simon-ibv.de>
2022-09-30 13:20:44 +0200

    Fix VerseList

--- src/components/VerseList.svelte
+++ src/components/VerseList.svelte
@@ -1,19 +1,28 @@
 <script lang="ts">
+    import { resultCount } from "../stores";
     import { getChapter } from "../api/bible";
     import Spinner from "./Spinner.svelte";
     import Verse from "./Verse.svelte";
+    // import { paginate, LightPaginationNav } from 'svelte-paginate';
 
     export var translation: String;
     export var book: Number = 0;
     export var chapter: Number = 0;
-    export var refs: Array<String> = [];
-    export var refsWithContent: Array<Object> = [];
+    export var refs: Array<String> | undefined = undefined;
+    export var refsWithContent: Array<Object> | undefined = undefined;
 
     let chapterResult: Promise<Object>;
+    // var items: any[] = [];
 
     $: if (translation && chapter >= 0 && book >= 0) {
         chapterResult = getChapter(translation, book, chapter);
     }
+
+    $: $resultCount = !!refs ? refs.length : !!refsWithContent ? refsWithContent.length : 0;
+    // $: items = refs || refsWithContent;
+    // let currentPage = 1
+    // let pageSize = 4
+    // $: paginatedItems = paginate({ items, pageSize, currentPage })
 </script>
 
 {#if !!refs}
@@ -24,6 +33,10 @@
     {#each refsWithContent as verseRef}
         <Verse translation={translation} {verseRef} />
     {/each}
+<!-- {#if !!paginatedItems}
+    {#each paginatedItems as verseRef}
+        <Verse translation={translation} {verseRef} />
+    {/each} -->
 {:else}
     {#await chapterResult}
         <Spinner/>
@@ -37,3 +50,12 @@
         {/if}
     {/await}
 {/if}
+
+<!-- <LightPaginationNav
+  totalItems="{items.length}"
+  pageSize="{pageSize}"
+  currentPage="{currentPage}"
+  limit="{1}"
+  showStepOptions="{true}"
+  on:setPage="{(e) => currentPage = e.detail.page}"
+/> -->
